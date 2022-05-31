a6dab27873417f811c66fd438f18a885abd02847
Laura Santi <89958677+Lauraitnas@users.noreply.github.com>
2022-05-31 19:11:06 +0800

    registered admin

--- groupProjectBackend/users/apps.py
+++ groupProjectBackend/users/apps.py
@@ -2,8 +2,8 @@ from django.apps import AppConfig
 from django.contrib.admin.apps import AdminConfig
 
 
-class MentorsAdminConfig(AdminConfig):
-    default_site = "blog.admin.BlogAdminArea"
+# class MentorsAdminConfig(AdminConfig):
+#     default_site = "blog.admin.BlogAdminArea"
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
